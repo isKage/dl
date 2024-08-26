3f4348a0132d8d7d25f2e062aa2eff5453881ad6
isKage <145343804+Kage-233@users.noreply.github.com>
2024-08-26 15:42:18 +0800

    访问参数

---
+++ builders_guide/parameter_access.py
@@ -0,0 +1,44 @@
+"""
+参数管理
+"""
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+import torch
+from torch import nn
+
+'''参数访问'''
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 1))
+X = torch.rand(size=(2, 4))
+net(X)
+print("=============================================================================")
+print(net[2].state_dict())
+print(type(net[2].bias))
+print(net[2].bias)
+print(net[2].bias.data)
+print(net[2].weight.grad)
+
+print(*[(name, param.shape) for name, param in net[0].named_parameters()])
+print(*[(name, param.shape) for name, param in net.named_parameters()])
+print("=============================================================================")
+
+'''从嵌套块收集参数'''
+
+
+def block1():
+    return nn.Sequential(nn.Linear(4, 8), nn.ReLU(),
+                         nn.Linear(8, 4), nn.ReLU())
+
+
+def block2():
+    net = nn.Sequential()
+    for i in range(4):
+        # 在这里嵌套
+        net.add_module(f'block {i}', block1())
+    return net
+
+
+rgnet = nn.Sequential(block2(), nn.Linear(4, 1))
+rgnet(X)
+print(rgnet)
